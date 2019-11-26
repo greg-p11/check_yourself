52d6d30171375a9eaf6a9796cd82fdf8a26c3f6f
gp <grzesiek.p12@wp.pl>
2019-11-26 18:45:27 +0100

    first commit

---
+++ js/data.js
@@ -0,0 +1,104 @@
+  var words=[ 
+    difficulty1=[
+      {"word":"actor", "result":"actor"},
+      {"word":"adult", "result":"adult"},
+      {"word":"basic", "result":"basic"},
+      {"word":"birth", "result":"birth"},
+      {"word":"grass", "result":"grass"},
+      {"word":"heavy", "result":"heavy"},
+      {"word":"humor", "result":"humor"},
+      {"word":"power", "result":"power"},
+      {"word":"proof", "result":"proof"},
+      {"word":"reply", "result":"reply"}
+    ],
+    difficulty2=[
+      {"word":"shadow", "result":"shadow"},
+      {"word":"simple", "result":"simple"},
+      {"word":"travel", "result":"travel"},
+      {"word":"visual", "result":"visual"},
+      {"word":"yellow", "result":"yellow"},
+      {"word":"junior", "result":"junior"},
+      {"word":"invite", "result":"invite"},
+      {"word":"hunter", "result":"hunter"},
+      {"word":"ground", "result":"ground"},
+      {"word":"freeze", "result":"freeze"}
+    ],
+    difficulty3=[
+      {"word":"numerous", "result":"numerous"},
+      {"word":"unlikely", "result":"unlikely"},
+      {"word":"whatever", "result":"whatever"},
+      {"word":"withdraw", "result":"withdraw"},
+      {"word":"normally", "result":"normally"},
+      {"word":"moderate", "result":"moderate"},
+      {"word":"function", "result":"function"},
+      {"word":"invasion", "result":"invasion"},
+      {"word":"homeless", "result":"homeless"},
+      {"word":"graduate", "result":"graduate"}
+    ],
+    difficulty4=[
+      {"word":"environment", "result":"environment"},
+      {"word":"dramatically", "result":"dramatically"},
+      {"word":"corporation", "result":"corporation"},
+      {"word":"arrangement", "result":"arrangement"},
+      {"word":"approximately", "result":"approximately"},
+      {"word":"occasionally", "result":"occasionally"},
+      {"word":"participation", "result":"participation"},
+      {"word":"recommendation", "result":"recommendation"},
+      {"word":"vulnerable", "result":"vulnerable"},
+      {"word":"unfortunately", "result":"unfortunately"}
+    ]  
+  ];
+
+  var  equations=[
+    difficulty1=[
+        {"word":"4+4", "result":"8"},
+        {"word":"3+8", "result":"11"},
+        {"word":"7+2", "result":"9"},
+        {"word":"8+2", "result":"10"},
+        {"word":"1+3", "result":"4"},
+        {"word":"6-2", "result":"4"},
+        {"word":"8-7", "result":"1"},
+        {"word":"6-1", "result":"5"},
+        {"word":"5-5", "result":"0"},
+        {"word":"4-2", "result":"2"}
+    ],
+    difficulty2=[
+        {"word":"4 x 5", "result":"20"},
+        {"word":"3 x 9", "result":"27"},
+        {"word":"6 x 4", "result":"24"},
+        {"word":"3 x 3", "result":"9"},
+        {"word":"9 x 8", "result":"72"},
+        {"word":"33 / 3", "result":"11"},
+        {"word":"42 / 7", "result":"6"},
+        {"word":"12 / 4", "result":"3"},
+        {"word":"21 / 7", "result":"3"},
+        {"word":"6 / 3", "result":"2"}
+    ], 
+    difficulty3=[
+        {"word":"(4+9)x3", "result":"39"},
+        {"word":"(7-1)x5", "result":"30"},
+        {"word":"(3-3)x2", "result":"0"},
+        {"word":"(6+8)x2", "result":"28"},
+        {"word":"(2+5)x6", "result":"42"},
+        {"word":"40/(2+2)", "result":"10"},
+        {"word":"36/(9-6)", "result":"12"},
+        {"word":"(22-4)/9", "result":"2"},
+        {"word":"(12+3)/3", "result":"5"},
+        {"word":"(14-4)/5", "result":"2"}
+    ],
+    difficulty4=[
+        {"word":"(11-4)x(3+4)", "result":"49"},
+        {"word":"(6+2)x(12-6)", "result":"48"},
+        {"word":"(28-12)x(6-4)", "result":"32"},
+        {"word":"(8-5+12)x3", "result":"45"},
+        {"word":"(47-15-16)/4", "result":"4"},
+        {"word":"(7+21+27)/5", "result":"11"},
+        {"word":"(3x6/2)+12", "result":"21"},
+        {"word":"(11x5)/(3+8)", "result":"5"},
+        {"word":"(28/4/7)x(23-4)", "result":"19"},
+        {"word":"(81/9)x(12/3)", "result":"36"}
+    ]
+  ];
+
+
+    
---
+++ js/script.js
@@ -0,0 +1,165 @@
+$("body").ready(function () {
+    $("#btnwrite").css({ 'background-color': 'rgb(255, 147, 5)' });
+    $("#btncalc").css({ 'background-color': 'rgb(202, 116, 4)' });
+    $("#btnwrite").val(1);
+    $("#btncalc").val(0);
+    $("#contentType").html("Press Start and then within 5 seconds write the word that will be displayed below.");
+    let bestResultWriting = 'BestcoreWriting';
+    let bestResult = sessionStorage.getItem(bestResultWriting); 
+    $("#bestScore").html(bestResult);
+});
+
+$("#btnwrite").click(function () {
+    $("#btnwrite").css({ 'background-color': 'rgb(255, 147, 5)' });
+    $("#btncalc").css({ 'background-color': 'rgb(202, 116, 4)' });
+    $("#btnwrite").val(1);
+    $("#btncalc").val(0);
+    $("#contentType").html("Press Start and then within 5 seconds write the word that will be displayed below.");
+    let bestResultWriting = 'BestcoreWriting';
+    let bestResult = sessionStorage.getItem(bestResultWriting); 
+    $("#bestScore").html(bestResult);
+});
+
+$("#btncalc").click(function () {
+    $("#btncalc").css({ 'background-color': 'rgb(255, 147, 5)' });
+    $("#btnwrite").css({ 'background-color': 'rgb(202, 116, 4)' });
+    $("#btncalc").val(1);
+    $("#btnwrite").val(0);
+    $("#contentType").html("Press Start and then within 5 seconds enter the result of the equation, which will be displayed below.");  
+    let bestResultCalculation= 'Calculation';
+    let bestResult = sessionStorage.getItem(bestResultCalculation); 
+    $("#bestScore").html(bestResult); 
+
+});
+
+$("#startTask").click(function () {
+    $("#startTask").attr("disabled", true);
+    $("#answer").focus();
+    if ($("#btnwrite").val() == 1) 
+    {   
+        let bestResultWriting = 'BestcoreWriting';
+        let bestResult = sessionStorage.getItem(bestResultWriting); 
+        $("#bestScore").html(bestResult);  
+        test(words, bestResultWriting);   
+    }
+    else if ($("#btncalc").val() == 1)
+    {
+        let bestResultCalculation= 'Calculation';;
+        let bestResult = sessionStorage.getItem(bestResultCalculation); 
+        $("#bestScore").html(bestResult); 
+        test(equations, bestResultCalculation);
+    }
+    else
+        alert("SOMETHING GOES WRONG! \rPlease refresh this page.");
+});
+
+const test = (content, best) => {
+
+    var bestResult = sessionStorage.getItem(best); 
+      
+    if (bestResult>0){
+        var bestScore = bestResult
+        $("#bestScore").html(bestResult);      
+    }
+    else{
+        var bestScore = 0;
+        $("#bestScore").html();    
+    }
+
+    let difficult = 0;
+    let quest = content[difficult][Math.floor(content[difficult].length * Math.random())];
+    let lastQuest = quest.word;
+
+    $("#question").html(quest.word);
+    let i=0;
+    
+    $("#answer").keyup(function () {
+            var scoreVal = parseInt($("#score").html()); 
+            
+            if(i==0){               
+                timer(true);
+                timeBar(false);
+                i=1;
+            }
+        
+            let answVal =  $("#answer").val().toLowerCase();
+
+
+            if (quest.result == answVal)
+            {
+                timer(false);
+               // timeBar(true);
+
+                $(".timeBar").remove();
+               
+                $("#answer").val("");
+
+                scoreVal = scoreVal+1;
+                $("#score").html(scoreVal);
+
+                if(scoreVal%5==0 && scoreVal<=20)
+                {
+                    difficult = difficult+1;
+                }
+
+                //if który w przypadku posiadania powyżej 20 pkt bedzie losował słowo ze wszystkich słów(lub działań) w pliku data.js
+                if(scoreVal>20)
+                {
+                    difficult = Math.floor(4*Math.random());
+                }
+
+                while(lastQuest==quest.word)
+                {
+                    quest = content[difficult][Math.floor(content[difficult].length * Math.random())];   
+                }
+                lastQuest=quest.word;
+                
+                $("#question").html(quest.word);
+                 
+                timer(true);
+                //timeBar(false);
+                
+                //w ms edge nie działała w pełni poprawnie funkcja timebar dlaego został zastosowany sposób z remove i append
+                $(".sideBarLeft").append('<div id="timeBar" class="timeBar"></div>');
+                $(".sideBarRight").append('<div class="timeBar"></div>');
+
+                if(scoreVal>bestScore){
+                    $("#bestScore").html(scoreVal);
+                    sessionStorage.setItem(best, scoreVal);
+                }
+            } 
+        });
+    }
+    
+const timeLeft = () =>{
+    
+    $("#yourScore").html("Your score: "+$("#score").html());
+    $("#loseWindow").attr("hidden", false);
+    $("#answer").attr("disabled", true);   
+}
+
+const timer = (start) =>{
+    if(start==true){ timeStart = setTimeout(timeLeft, 5000);}
+    if(start==false){clearTimeout(timeStart);}
+}
+
+const timeBar = (startBar) =>{
+    if(startBar==true){$(".timeBar").attr("hidden", true);}
+    if(startBar==false){$(".timeBar").attr("hidden", false);}
+}
+
+$("#playAgain").click(function () {     
+    timeBar(true);
+    $("#startTask").attr("disabled", false);
+    $("#question").html("Be ready");
+    $("#score").html("0"); 
+    $("#answer").val(""); 
+    $("#loseContent").animate({height: "0%"},3000)
+    $(".lostInfo").animate({opacity: "0"},3000)
+    setTimeout(function(){$("#loseWindow").attr("hidden", true); }, 3000)
+    $("#loseContent").animate({height: "100%"})
+    $(".lostInfo").animate({opacity: "100"})   
+    $("#answer").attr("disabled", false);
+});
+
+
